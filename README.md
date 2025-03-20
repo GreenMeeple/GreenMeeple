8a000e7e66980100d64f529c783df5393ac15f72
Li Chun Ngai, Alex <alexcnli@yahoo.com>
2025-03-20 20:40:54 +0100

    update script

--- README.md
+++ README.md
@@ -1,16 +1,33 @@
-## Hi there 👋
+  
 
-<!--
-**GreenMeeple/GreenMeeple** is a ✨ _special_ ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+<p align="center"> M.Sc Cybersecurity at Saarland University, B.Eng Information Engineering at CUHK. </p>  
+<p align="center"> Experienced in C++, Python, and Full Stack Development </p>  
+<p align="center"> Cybersecurity Analyst, focus on LLM security.</p>  
 
-Here are some ideas to get you started:
 
-- 🔭 I’m currently working on ...
-- 🌱 I’m currently learning ...
-- 👯 I’m looking to collaborate on ...
-- 🤔 I’m looking for help with ...
-- 💬 Ask me about ...
-- 📫 How to reach me: ...
-- 😄 Pronouns: ...
-- ⚡ Fun fact: ...
--->
+<table align="center"><tr>
+<td valign="top" width="33%">
+
+### Projects and Repos  
+- [Azul_Test](https://github.com/xindoo/eng-practices-cn)A C++ program based on a boardgame Azul by Michael Kiesling.
+- [MensaarLecker](https://github.com/GreenMeeple/MensaarLecker)A Web Crawler developed with Python and Selenium
+- [hexo-zhruby](https://github.com/GreenMeeple/hexo-zhruby) A Hexo Tag plugin developed using Node.js
+- [Leetcode](https://github.com/GreenMeeple/Leetcode/index) LeetCode solution with Explanation  
+   
+[More](https://github.com/GreenMeeple/)	 
+
+	
+</td>
+<td valign="top" width="33%">
+
+### Personal Blog
+- [Automata, Games, and Verification (Portal)](https://greenmeeple.github.io/AGV/agv/)
+- [Leetcode Walkthrough (Portal 🚪)](https://greenmeeple.github.io/LeetCode/index/)
+- [3-hexo Shortcut](https://greenmeeple.github.io/3_hexo_Shortcut/)
+- [AGV 12.3 -- Complementation of Parity Tree Automata](https://greenmeeple.github.io/AGV/agv12-3/)
+- [AGV 12.2 -- Emptiness Game](https://greenmeeple.github.io/AGV/agv12-2/)
+
+[More Posts](https://greenmeeple.github.io/)
+
+</td>
+</tr></table>
---
+++ generateReadme.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+import urllib3
+from lxml import etree
+import html
+import re
+
+blogUrl = 'https://greenmeeple.github.io/'
+
+headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'} 
+
+
+# <p align="center">
+#   <img src="https://github.com/greenmeeple/greenmeeple/blob/output/github-contribution-grid-snake.svg"/>
+# </p>	
+
+
+def addIntro(f):
+	txt = '''  
+
+<p align="center"> M.Sc Cybersecurity at Saarland University, B.Eng Information Engineering at CUHK. </p>  
+<p align="center"> Experienced in C++, Python, and Full Stack Development </p>  
+<p align="center"> Cybersecurity Analyst, focus on LLM security.</p>  
+
+
+''' 
+
+	f.write(txt)
+
+def addProjectInfo(f):
+	txt ='''
+### Projects and Repos  
+- [Azul_Test](https://github.com/xindoo/eng-practices-cn)A C++ program based on a boardgame Azul by Michael Kiesling.
+- [MensaarLecker](https://github.com/GreenMeeple/MensaarLecker)A Web Crawler developed with Python and Selenium
+- [hexo-zhruby](https://github.com/GreenMeeple/hexo-zhruby) A Hexo Tag plugin developed using Node.js
+- [Leetcode](https://github.com/GreenMeeple/Leetcode/index) LeetCode solution with Explanation  
+   
+[More](https://github.com/GreenMeeple/)	 
+
+	''' 
+	f.write(txt) 
+
+def addBlogInfo(f):  
+	http = urllib3.PoolManager(num_pools=5, headers=headers)
+	resp = http.request('GET', blogUrl)
+	resp_tree = etree.HTML(resp.data.decode("utf-8"))
+
+	# Corrected XPath to select individual blog post links
+	html_data = resp_tree.xpath(".//nav[@id='title-list-nav']/a")
+	f.write("\n### Personal Blog\n")
+
+	cnt = 0
+	for i in html_data: 
+		if cnt >= 5:
+			break
+
+		# Extract title
+		title_list = i.xpath("./span[@class='post-title']/text()")
+		if not title_list:
+			print("Skipping an entry due to missing title")
+			continue  # Skip if title is missing
+		title = title_list[0].strip()
+		print(title)
+
+		# Extract URL
+		url_list = i.xpath('./@href')
+		if not url_list:
+			print("Skipping an entry due to missing URL")
+			continue  # Skip if URL is missing
+		url = url_list[0]  
+
+		# Append full URL if it's relative
+		if not url.startswith("http"):
+			url = f"https://greenmeeple.github.io{url}"
+
+		item = f'- [{title}]({url})\n'
+		f.write(item)
+		cnt += 1
+
+	f.write('\n[More Posts](https://greenmeeple.github.io/)\n')
+
+if __name__=='__main__':
+	f = open('README.md', 'w+',encoding='utf-8')
+	addIntro(f)
+	f.write('<table align="center"><tr>\n')
+	f.write('<td valign="top" width="33%">\n')
+	addProjectInfo(f)
+	f.write('\n</td>\n')
+	f.write('<td valign="top" width="33%">\n')
+	addBlogInfo(f)
+	f.write('\n</td>\n')
+	f.write('</tr></table>\n')
+	f.close 
+
